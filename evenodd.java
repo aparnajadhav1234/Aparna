3c18854eddc783ff64bc363df52199fa83896ee7
aparnajadhav1234 <japarna300@gmail.com>
2025-07-24 15:48:57 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class evenodd
+{
+public static void main(String args[])
+{
+ int num;
+Scanner sc=new Scanner (System.in);
+num=sc.nextInt();
+if(num %2==0)
+   {
+    System.out.println("number is even"  +num);
+  }
+else
+{
+ System.out.println("number is odd"  +num);
+}
+
+}
+
+
+}
