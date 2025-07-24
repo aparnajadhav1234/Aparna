70383256f84140c383ee96b1f04e12ba9d1271eb
aparnajadhav1234 <japarna300@gmail.com>
2025-07-24 16:04:27 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class swap{
+public static void main(String args[]){
+int a,b,temp;
+Scanner sc=new Scanner(System.in);
+a= sc.nextInt();
+b=sc.nextInt();
+
+System.out.println("before swapping number" +a);
+System.out.println("before swapping number" +b);
+
+temp=a;
+a=b;
+b=temp;
+System.out.println("swapping number a"  +a);
+System.out.println("swapping number b " +b);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
