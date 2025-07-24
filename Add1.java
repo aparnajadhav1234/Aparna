c74ac6446387bc61c57e2179ab183f96e9a68a0a
aparnajadhav1234 <japarna300@gmail.com>
2025-07-24 15:41:06 +0530

    Add files via upload

---
+++ Add1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Add1
+{
+ public static void main(String args[])
+  {
+    int a,b,c;
+ System.out.println("Enter the frist Number");
+Scanner sc= new Scanner(System.in);
+a=sc.nextInt();
+System.out.println("Enter the second Number");
+b=sc.nextInt();
+c=a+b;
+System.out.println("Addition is"+c);
+   }
+}
